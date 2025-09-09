93e73df99d617d319068fa5bea46cbbe9ef81a01
rodney334 <rodneyivan334@gmail.com>
2025-09-09 18:34:30 +0100

    correction

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 // /pages/api/auth/[...nextauth].ts
-import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth';
+// import NextAuth from "next-auth";
+// import { authOptions } from '@/lib/auth';
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
